ba5aa5ea139856ffdd1b9124c5e18cf63caa76a5
KimHong Poun <159880356+Kimhong-29@users.noreply.github.com>
2025-02-02 05:41:42 +0000

    Ready for grading.

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# KimHong Poun
+# 01/31/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
@@ -9,6 +9,12 @@
 
 # Get the amount of projected sales.
 
+total_sales = float(input("Enter the projected sale: "))
+
 # Calculate the projected profit.
 
+profit = total_sales * 0.23
+
 # Print the projected profit.
+
+print(f"The profit is $", format(profit, ',.2f'))
