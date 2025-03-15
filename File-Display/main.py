2ffee0af38ed19d3bdbf58df4baf3827cf8566b5
KimHong Poun <159880356+Kimhong-29@users.noreply.github.com>
2025-03-15 20:01:24 +0000

    Ready for grading.

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,17 @@
 #
-# Name
-# Date
+# KimHong Poun
+# 03/15/2025
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+# Open the file.
+myfile = open('numbers.txt', 'r')
+
+# Read and display the file's contents.
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close the file.
+myfile.close()
