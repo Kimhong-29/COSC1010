af196beb3b05a23bddb3e88fc2b4056582be6a05
KimHong Poun <159880356+Kimhong-29@users.noreply.github.com>
2025-03-15 20:21:40 +0000

    Ready for grading.

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,25 @@
 #
-# Name
-# Date
+# KimHong Poun
+# 03/15/2025
 # Average of Numbers Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+myfile = open('numbers.txt', 'r')
+
+# Initialize variables for sum and count.
+total_sum = 0
+count = 0
+
+# Read and display the file's contents.
+for line in myfile:
+    number = int(line)
+    total_sum += number
+    count += 1
+    average = total_sum / count
+
+# Close the file.
+myfile.close()
+
+# Display the average.
+print(f"The average of the numbers is: {average}")
