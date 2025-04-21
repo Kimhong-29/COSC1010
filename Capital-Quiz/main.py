7a96600bde18559b15c22d1eafbf237b3e3567b6
KimHong Poun <159880356+Kimhong-29@users.noreply.github.com>
2025-04-21 02:54:41 +0000

    Ready for grading.

--- Capital-Quiz/main.py
+++ Capital-Quiz/main.py
@@ -1,16 +1,14 @@
 #
-# Name
-# Date
+# KimHong Poun
+# 04/20/2025
 # Capital Quiz Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
-
 import random
+def state_cap_dictionary():
 
-def main():
-    # Initialize dictionary
-    capitals = {'Alabama':'Montgomery', 'Alaska':'Juneau',
+        sc = {'Alabama':'Montgomery', 'Alaska':'Juneau',
                 'Arizona':'Phoenix', 'Arkansas':'Little Rock',
                 'California':'Sacramento', 'Colorado':'Denver',
                 'Connecticut':'Hartford', 'Delaware':'Dover',
@@ -35,6 +33,38 @@ def main():
                 'Vermont':'Montpelier', 'Virginia':'Richmond',
                 'Washington':'Olympia', 'West Virginia':'Charleston',
                 'Wisconsin':'Madison', 'Wyoming':'Cheyenne'}
+        return sc
+
+NUM_STATES = 5
+def main():
+    # Initialize dictionary
+    state_caps = state_cap_dictionary()
+
+    # Initial varibles to keep count of the number 
+    # of correct and incorrect answer.
+    correct = 0
+    incorrect = 0
+
+    # Quiz the user.
+    for count in range (NUM_STATES):
+        # Get a random entry from the dictionary
+        state, capital = state_caps.popitem()
+
+        # Quiz the user.
+        print('What is the capital of ', state, '? ', end=' ')
+        response = input()
+
+        # Is the user correct?
+        if response.lower() == capital.lower():
+            correct += 1
+            print('Correct!')
+        else:
+            incorrect += 1
+            print('Incorrect!')
+
+    # Display the results.
+    print('Correct responses:', correct)
+    print('Incorrect responses:', incorrect)
 
     # Local variables
 
